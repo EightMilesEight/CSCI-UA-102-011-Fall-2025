9f967850daa4c15f9a66897ba1a6043a7dc75722
8Miles8 <miles@8miles8.dev>
2025-09-21 17:39:05 -0400

    Finished Assignment 1

--- code/Java review/OOPReview/EncapsulationExample.java
+++ code/Java review/OOPReview/EncapsulationExample.java
@@ -1,27 +1,27 @@
 package OOPReview;
 
 class Circle {
-	public double radius;
-	public double area;
-	public static double pi = 3.14159;
-	
-	public Circle(double radius) {
-		this.radius = radius;
-		this.area = pi * radius * radius;
-	}
+    public static double pi = 3.14159;
+    public double radius;
+    public double area;
+
+    public Circle(double radius) {
+        this.radius = radius;
+        this.area = pi * radius * radius;
+    }
 }
 
 
 public class EncapsulationExample {
-	public static void main(String [] args) {
-		Circle c1 = new Circle(1.);
-		Circle c2 = new Circle(2.);
-		
-		System.out.println("radius is " + c1.radius);
-		System.out.println("area is " + c1.area);
-		System.out.println("radius is " + c2.radius);
-		System.out.println("area is " + c2.area);
-		
+    public static void main(String[] args) {
+        Circle c1 = new Circle(1.);
+        Circle c2 = new Circle(2.);
+
+        System.out.println("radius is " + c1.radius);
+        System.out.println("area is " + c1.area);
+        System.out.println("radius is " + c2.radius);
+        System.out.println("area is " + c2.area);
+
 ////		Is this a good way of copying a circle if
 ////		we do not want changes to c2 to affect c1?
 //		c2 = c1;
@@ -37,5 +37,5 @@ public class EncapsulationExample {
 //		
 //		System.out.println("radius is " + c1.getRadius());
 //		System.out.println("area is " + c1.getArea());
-	}
+    }
 }
